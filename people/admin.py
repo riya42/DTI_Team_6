330e67a4636b2cd363d239e57f664b2a663836d1
riya42 <114979528+riya42@users.noreply.github.com>
2024-04-24 03:47:27 +0530

    Delete people directory

--- people/admin.py
+++
@@ -1,9 +0,0 @@
-from email.errors import MissingHeaderBodySeparatorDefect
-from django.contrib import admin
-
-# Register your models here.
-from .models import MissingPerson, ReportedPerson
-
-admin.site.register(MissingPerson)
-
-admin.site.register(ReportedPerson)
